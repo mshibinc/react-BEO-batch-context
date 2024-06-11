4e01f9b375055d756574bd30434382831fe2f249
Shibin Shibin <shibinshibin@Shibins-MacBook-Pro.local>
2024-06-11 20:47:38 +0530

    axios get one

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,16 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axiosConfig from '../utils/axiosFetch'
 
 export default function Products() {
-    useEffect(()=>{
+    const [products, setProducts] = useState([])
+    const [productsDetails, setProductsDetails] = useState(null)
+    useEffect(() => {
         getProducts();
-    },[])
-    const getProducts =()=>{
-        axiosConfig.get('/objects').then(res=>{
-            console.log(res);
+    }, [])
+    const getProducts = () => {
+        axiosConfig.get('/objects').then(res => {
+            setProducts(res);
+        }).catch((err) => {
+            alert('err')
         })
     }
-  return (
-    <div>Products</div>
-  )
+    const showProduct = (val) => {
+        axiosConfig.get(`/objects?id=${val}`).then(res => {
+            setProductsDetails(...res);
+        }).catch((err) => {
+            alert('err')
+        })
+    }
+    return (
+        <div>
+            {productsDetails!=null&&<div>
+                <p>{productsDetails.name}</p>
+                <p>{productsDetails?.data?.capacity}</p>
+                <p>{productsDetails?.data?.color}</p>
+            </div>}
+            {products.map((itm, i) => (
+                <div>
+                    <h4>{itm.name}</h4>
+                    <button onClick={() => showProduct(itm.id)}>Show Details</button>
+                </div>
+            ))}
+        </div>
+    )
 }
