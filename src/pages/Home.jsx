49e72785aa2e64fba7dbbeba816288412a170153
Shibin Shibin <shibinshibin@Shibins-MacBook-Pro.local>
2024-06-19 20:58:02 +0530

    styledcmpt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useStateValue } from '../contextAPI/GlobelState'
 import Button from '../components/Button';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom'
 export default function Home() {
     const [{ users, place }, dispatch] = useStateValue();
     // console.log(users,place);
@@ -14,20 +16,47 @@ export default function Home() {
         dispatch({ type: 'add', data: { name: 'Sonu', age: 12 } })
     }
     return (
-        <div>Home
-            <br />
+        <Wrapper>
+            <h2 className='head-2'>Home</h2>
+            <Para>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam laborum minima reiciendis quidem iusto quod magnam quas vitae. Ut porro repudiandae eligendi suscipit non similique numquam consequuntur est perspiciatis repellendus?</Para>
+            <h3>My h3</h3>
+            <MyLink to="/products">products</MyLink>
             {/* <button onClick={adduserTo}>Add user</button><br/> */}
             {/* isInfo */}
-            <Button name="Add user"  type="success" onClick={adduserTo}/><br/>
-            {/* <Button name="Add user"  type="info"/> */}
+            <Button name="Add user" type="success" onClick={adduserTo} /><br />
+            <MyButton name="MyButton" type="success" />
             {users.map((itm, i) => (
                 <Usercard data={itm} key={i} />
             ))}
-        </div>
+        </Wrapper>
     )
 }
 
-    
+const Wrapper = styled.div`
+font-size: 14px;
+        .head-2{
+            color: red;
+        }
+        .head-2:hover{
+            color: blue;
+        }
+        h3{
+            color:blue
+        }
+    `;
+const Para = styled.p`
+        font-size: 20px;
+    `
+const MyLink = styled(Link)`
+        text-decoration: none;
+        color: green;
+        text-transform:uppercase;
+    `
+const MyButton = styled(Button)`
+    text-decoration: none;
+    color: green;
+    text-transform:uppercase;
+`
 // `;
 // const Button = styled.button({
 //      backgroundColor:'red'
